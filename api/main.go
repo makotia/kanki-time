2d59aac092ea9905570ee83630e11c9f28ac28d3
makotia <3mdevmail@gmail.com>
2021-01-13 11:47:09 +0900

    :+1: fix path

--- api/main.go
+++ api/main.go
@@ -15,25 +15,15 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/create", create)
-	http.HandleFunc("/", get)
+	http.HandleFunc("/api/", handler)
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func create(w http.ResponseWriter, r *http.Request) {
-	type reqJSON struct {
-		Text string `json:"Text"`
-		Type string `json:"Type"`
-	}
-
+func handler(w http.ResponseWriter, r *http.Request) {
 	var (
-		id        string
-		err       error
-		b         []byte
-		returnMap map[string]string
-		reqBody   reqJSON
-		buf       bytes.Buffer
+		err error
+		p   []string
 	)
 
 	w.Header().Set("Access-Control-Allow-Headers", "*")
@@ -41,43 +31,8 @@ func create(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Content-Type", "application/json")
 
-	if r.Method != "POST" {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	io.Copy(&buf, r.Body)
-
-	if err = json.Unmarshal(buf.Bytes(), &reqBody); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if id, err = util.GenImage(reqBody.Text, reqBody.Type); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	returnMap = map[string]string{
-		"id":    id,
-		"error": "",
-	}
-
-	if b, err = json.Marshal(returnMap); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprint(w, string(b))
-}
-
-func get(w http.ResponseWriter, r *http.Request) {
-	var (
-		err error
-		p   []string
-	)
-	if r.Method != "GET" {
-		w.WriteHeader(http.StatusNotAcceptable)
-	} else {
+	switch r.Method {
+	case "GET":
 		p = strings.Split(r.URL.Path, "/")
 		if _, err = os.Stat(path.Join(config.GetConfig().Server.StaticDir, p[len(p)-1]+".png")); err != nil {
 			if os.IsNotExist(err) {
@@ -85,5 +40,48 @@ func get(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 		}
+	case "POST":
+		type reqJSON struct {
+			Text string `json:"Text"`
+			Type string `json:"Type"`
+		}
+
+		var (
+			id        string
+			b         []byte
+			returnMap map[string]string
+			reqBody   reqJSON
+			buf       bytes.Buffer
+		)
+
+		io.Copy(&buf, r.Body)
+
+		if err = json.Unmarshal(buf.Bytes(), &reqBody); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		if id, err = util.GenImage(reqBody.Text, reqBody.Type); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		returnMap = map[string]string{
+			"id":    id,
+			"error": "",
+		}
+
+		if b, err = json.Marshal(returnMap); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprint(w, string(b))
+	case "HEAD":
+		w.WriteHeader(http.StatusOK)
+	case "OPTIONS":
+		w.WriteHeader(http.StatusOK)
+	default:
+		fmt.Println(r.Method)
+		w.WriteHeader(http.StatusNotAcceptable)
 	}
 }
