66ee8cbfdbbb686fa4bb836ed1358aa7a3ebff06
makotia <3mdevmail@gmail.com>
2021-01-13 16:24:52 +0900

    :-1: わからない

--- front/src/pages/[id]/index.jsx
+++ front/src/pages/[id]/index.jsx
@@ -7,38 +7,42 @@ import fetcher from '../../lib/fetcher'
 import styles from '../../styles/Home.module.css'
 import Meta from '../../components/meta'
 
-export default function Home() {
-  const router = useRouter()
-  const [shouldFetch, setShouldFetch] = useState(false)
-  const { id } = router.query
-  useEffect(() => {
-    setShouldFetch(true)
-  }, [id])
-  const imgUrl = `${process.env.apiUrl}/api/media/${id}.png`
-  const apiUrl = process.env.apiUrl
-  const { error } = useSWR(
-    shouldFetch ? [`${process.env.apiUrl}/api/${id}`, ''] : null,
-    fetcher
-  )
+export default function Home({ id, apiUrl, imgUrl, succeed }) {
+  console.log(id)
+  console.log(apiUrl)
+  console.log(imgUrl)
+  console.log(succeed)
   return (
     <div>
       <Meta image={imgUrl} url={apiUrl} />
-      {(!error && imgUrl) && <>
-        <p>Fuck</p>
-      </>}
-      {error && router.push('/')}
+      {/* {!succeed && router.push('/')} */}
     </div>
   )
 }
 
-export const getServerSideProps = async (context) => {
-  const { id } = context.query
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id
+
+  const { error } = useSWR(
+    shouldFetch ? [`${process.env.apiUrl}/api/${id}`, ''] : null,
+    fetcher
+  )
   const apiUrl = process.env.API_URL
+
   return {
     props: {
       id: id,
       apiUrl: apiUrl,
       imgUrl: `${apiUrl}/api/media/${id}.png`,
+      succeed: !error,
     },
+    revalidate: 60,
   }
 }
