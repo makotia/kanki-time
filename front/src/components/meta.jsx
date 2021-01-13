aa8c09ada0a57779612735ac14af3557a866302c
makotia <3mdevmail@gmail.com>
2021-01-13 15:49:17 +0900

    :+1: キチガイ

--- front/src/components/meta.jsx
+++ front/src/components/meta.jsx
@@ -8,15 +8,15 @@ const Meta = ({ image, url }) => {
       <link rel="icon" href="/favicon.ico" />
       <meta property="og:title" content="換気タイム" />
       <meta property="og:site_name" content="換気タイム" />
-      <meta property="twitter:title" content="換気タイム" />
-      <meta property="twitter:description" content="換気タイム" />
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:site" content="@0x307E" />
-      <meta property="twitter:url" content={url} />
+      <meta name="twitter:title" content="換気タイム" />
+      <meta name="twitter:description" content="換気タイム" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@0x307E" />
+      <meta name="twitter:url" content={url} />
       <meta property="og:url" content={url} />
       <meta property="og:description" content="換気タイム" />
       <meta property="og:image" content={image} />
-      <meta property="twitter:image" content={image} />
+      <meta name="twitter:image" content={image} />
     </Head>
   )
 }
