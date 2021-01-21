8228b5a690e32292aa41ebe4508179e121ceed36
makotia <3mdevmail@gmail.com>
2021-01-21 12:48:33 +0900

    :sparkle: echo

--- api/go.mod
+++ api/go.mod
@@ -4,7 +4,14 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/labstack/gommon v0.3.0
+	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/oklog/ulid v1.3.1
+	github.com/valyala/fasttemplate v1.2.1
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/image v0.0.0-20201208152932-35266b937fa6
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- api/main.go
+++ api/main.go
@@ -9,95 +9,159 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"strings"
+	"sync"
+	"time"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/color"
 	"github.com/makotia/kanki-time/api/config"
 	"github.com/makotia/kanki-time/api/util"
+	"github.com/valyala/fasttemplate"
 )
 
 func main() {
 	var addr = flag.String("addr", ":8080", "TCP address to listen to")
+	e := echo.New()
+	e.Use(middleware.RequestID())
+	e.Use(logger())
+	e.Use(middleware.Recover())
 
-	flag.Parse()
-
-	http.HandleFunc("/", defaultHandler)
-	http.HandleFunc("/api", handler)
-	http.HandleFunc("/api/", handler)
-	http.Handle("/api/media/", http.StripPrefix("/api/media/", http.FileServer(http.Dir(config.GetConfig().Server.StaticDir))))
-
-	http.ListenAndServe(*addr, nil)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodOptions},
+	}))
+	api := e.Group("/api")
+	{
+		api.GET("/:id", getHandler)
+		api.POST("/", createHandler)
+	}
+	e.Logger.Fatal(e.Start(*addr))
 }
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	w.WriteHeader(200)
+func getHandler(c echo.Context) (err error) {
+	if _, err = os.Stat(path.Join(config.GetConfig().Server.StaticDir, c.QueryParam("id")+".png")); err != nil {
+		if os.IsNotExist(err) {
+			return c.NoContent(http.StatusNotFound)
+		}
+	}
+	return c.NoContent(http.StatusOK)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func createHandler(c echo.Context) (err error) {
+	type reqJSON struct {
+		Text string `json:"Text"`
+		Type string `json:"Type"`
+	}
+
 	var (
-		err error
-		p   []string
+		id      string
+		reqBody reqJSON
+		buf     bytes.Buffer
 	)
 
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	io.Copy(&buf, c.Request().Body)
 
-	switch r.Method {
-	case "GET":
-		w.Header().Set("Content-Type", "application/json")
-		p = strings.Split(r.URL.Path, "/")
-		if _, err = os.Stat(path.Join(config.GetConfig().Server.StaticDir, p[len(p)-1]+".png")); err != nil {
-			if os.IsNotExist(err) {
-				w.WriteHeader(http.StatusNotFound)
+	if err = json.Unmarshal(buf.Bytes(), &reqBody); err != nil {
+		return c.JSON(http.StatusInternalServerError, toMap(id, err))
+	}
+
+	if id, err = util.GenImage(reqBody.Text, reqBody.Type); err != nil {
+		return c.JSON(http.StatusInternalServerError, toMap(id, err))
+	}
+	return c.JSON(http.StatusOK, toMap(id, err))
+}
+
+func toMap(id string, err error) map[string]string {
+	return map[string]string{
+		"id":    id,
+		"error": err.Error(),
+	}
+}
+
+type loggerConfig struct {
+	Format           string `yaml:"format"`
+	CustomTimeFormat string `yaml:"custom_time_format"`
+	Output           io.Writer
+
+	template *fasttemplate.Template
+	colorer  *color.Color
+	pool     *sync.Pool
+}
+
+func logger() echo.MiddlewareFunc {
+	config := loggerConfig{
+		Format:           "${time} | ${method} | ${status} | ${path}\n",
+		CustomTimeFormat: "15:04:05",
+		colorer:          color.New(),
+		Output:           os.Stdout,
+	}
+	config.template = fasttemplate.New(config.Format, "${", "}")
+	config.colorer = color.New()
+	config.colorer.SetOutput(config.Output)
+	config.pool = &sync.Pool{
+		New: func() interface{} {
+			return bytes.NewBuffer(make([]byte, 256))
+		},
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+			req := c.Request()
+			res := c.Response()
+			start := time.Now()
+			if err = next(c); err != nil {
+				c.Error(err)
+			}
+			stop := time.Now()
+			buf := config.pool.Get().(*bytes.Buffer)
+			buf.Reset()
+			defer config.pool.Put(buf)
+
+			if _, err = config.template.ExecuteFunc(buf, func(w io.Writer, tag string) (int, error) {
+				switch tag {
+				case "time":
+					return buf.WriteString(time.Now().Format(config.CustomTimeFormat))
+				case "method":
+					return buf.WriteString(fmt.Sprintf("%-7s", req.Method))
+				case "path":
+					p := req.URL.Path
+					if p == "" {
+						p = "/"
+					}
+					return buf.WriteString(p)
+				case "status":
+					n := res.Status
+					s := config.colorer.Green(n)
+					switch {
+					case n >= 500:
+						s = config.colorer.Red(n)
+					case n >= 400:
+						s = config.colorer.Yellow(n)
+					case n >= 300:
+						s = config.colorer.Cyan(n)
+					}
+					return buf.WriteString(s)
+				case "error":
+					if err != nil {
+						b, _ := json.Marshal(err.Error())
+						b = b[1 : len(b)-1]
+						return buf.Write(b)
+					}
+				case "latency_human":
+					return buf.WriteString(stop.Sub(start).String())
+				}
+				return 0, nil
+			}); err != nil {
 				return
 			}
-		}
-	case "POST":
-		w.Header().Set("Content-Type", "application/json")
-		type reqJSON struct {
-			Text string `json:"Text"`
-			Type string `json:"Type"`
-		}
 
-		var (
-			id        string
-			b         []byte
-			returnMap map[string]string
-			reqBody   reqJSON
-			buf       bytes.Buffer
-		)
-
-		io.Copy(&buf, r.Body)
-
-		if err = json.Unmarshal(buf.Bytes(), &reqBody); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			if config.Output == nil {
+				_, err = c.Logger().Output().Write(buf.Bytes())
+				return
+			}
+			_, err = config.Output.Write(buf.Bytes())
 			return
 		}
-
-		if id, err = util.GenImage(reqBody.Text, reqBody.Type); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		returnMap = map[string]string{
-			"id":    id,
-			"error": "",
-		}
-
-		if b, err = json.Marshal(returnMap); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprint(w, string(b))
-	case "HEAD":
-		w.WriteHeader(http.StatusOK)
-	case "OPTIONS":
-		w.WriteHeader(http.StatusOK)
-	default:
-		fmt.Println(r.Method)
-		w.WriteHeader(http.StatusNotAcceptable)
 	}
 }
