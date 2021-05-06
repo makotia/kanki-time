a3dcae3d841f0be397ce0253cd8a9ab16f3599c2
makotia <3mdevmail@gmail.com>
2021-05-06 17:00:38 +0900

    fix dups

--- front/src/pages/image/[type]/[text]/index.tsx
+++ front/src/pages/image/[type]/[text]/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import Head from 'next/head'
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
 import { Props } from '@/lib/props'
+import Meta from '@/components/meta'
 
 const Image: React.FC<Props> = ({ text, type }) => {
   const url = process.env.baseUrl
@@ -12,25 +12,7 @@ const Image: React.FC<Props> = ({ text, type }) => {
     const f = async () => router.push('/')
     f()
   }, [])
-  return (
-    <div>
-      <Head>
-        <title>換気タイム</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta property="og:title" content="換気タイム" />
-        <meta property="og:site_name" content="換気タイム" />
-        <meta name="twitter:title" content="換気タイム" />
-        <meta name="twitter:description" content="換気タイム" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@0x307E" />
-        <meta name="twitter:url" content={url} />
-        <meta property="og:url" content={url} />
-        <meta property="og:description" content="換気タイム" />
-        <meta property="og:image" content={imgURL} />
-        <meta name="twitter:image" content={imgURL} />
-      </Head>
-    </div>
-  )
+  return <Meta image={imgURL} url={url} />
 }
 
 export default Image
